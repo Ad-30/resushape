870de2b40887f9194317ee6296804e9c6818f556
Lunn <thafm.work@gmail.com>
2024-08-03 21:11:43 +0100

    Add GetStarted

---
+++ components/getstarted.tsx
@@ -0,0 +1,15 @@
+export function Getstarted() {
+    return (
+        <main className="w-full">
+            <section className="w-full h-screen flex items-center justify-center bg-gray-950 py-24 md:py-32 lg:py-40">
+                <div className="grid max-w-2xl grid-cols-1 gap-6 md:grid-cols-[1fr_auto]">
+                    <div className="space-y-4 text-center">
+                        <h1 className="text-4xl font-extrabold tracking-tight text-gray-50 sm:text-5xl md:text-6xl">
+                            Get Started
+                        </h1>
+                    </div>
+                </div>
+            </section>
+        </main>
+    );
+}
